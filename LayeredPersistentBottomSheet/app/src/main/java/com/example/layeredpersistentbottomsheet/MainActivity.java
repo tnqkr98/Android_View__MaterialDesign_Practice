5d174cb7b359c9378063d42c3e55360ff640ed6a
donghan <tnqkr98@naver.com>
2020-06-02 15:54:55 +0900

    Add Layered Persistent Bottom Sheets

---
+++ LayeredPersistentBottomSheet/app/src/main/java/com/example/layeredpersistentbottomsheet/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.layeredpersistentbottomsheet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomSheetBehavior mBottomSheetBehavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View bottomSheet1 = findViewById(R.id.bottom_sheet1);
+        View bottomSheet2 = findViewById(R.id.bottom_sheet2);
+
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet1);
+
+        Button buttonExpand = (Button)findViewById(R.id.expand);
+        Button buttonCollapse = (Button)findViewById(R.id.collapse);
+
+        buttonExpand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        buttonCollapse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+
+        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+                switch(i){
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        break;
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
+            }
+        });
+    }
+}
