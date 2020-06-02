33bda51a95b34118105ff39f140f26c3c0cff1e9
donghan <tnqkr98@naver.com>
2020-06-02 11:22:10 +0900

    Bottom Navigation View + ViewPager Custom

--- bottomnavigation_custom/app/src/main/java/com/example/androidmaterial_practice/MainActivity.java
+++ bottomnavigation_custom/app/src/main/java/com/example/androidmaterial_practice/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.androidmaterial_practice;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
@@ -19,15 +18,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             switch (menuItem.getItemId()){
-                case R.id.menu1:
-                    viewPager.setCurrentItem(0);
-                    return true;
-                case R.id.menu2:
-                    viewPager.setCurrentItem(1);
-                    return true;
-                case R.id.menu3:
-                    viewPager.setCurrentItem(2);
-                    return true;
+                case R.id.menu1: viewPager.setCurrentItem(0);return true;
+                case R.id.menu2: viewPager.setCurrentItem(1);return true;
+                case R.id.menu3: viewPager.setCurrentItem(2);return true;
             }
             return false;
         }
@@ -38,20 +31,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager = (ViewPager)findViewById(R.id.viewpager);
         navigation = (BottomNavigationView)findViewById(R.id.bottomNavigationView);
-        //navigation.setItemIconTintList(null);
         navigation.setOnNavigationItemSelectedListener(mOnNavi);
 
         adapter = new Adapter(getSupportFragmentManager());
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-
         adapter.AddFragment(new Fragment1(),"1");
         adapter.AddFragment(new Fragment2(),"2");
         adapter.AddFragment(new Fragment3(),"3");
-        fragmentTransaction.commit();
-        viewPager.setAdapter(adapter);
 
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) { }
